45bc716c30f9bdb710c56a12a1383df5e7c42963
akatank <jk563htc@gmail.com>
2014-01-17 19:56:03 +0000

    Added test for testing two digit numbers.

--- integer_arithmetics/helper.py
+++ integer_arithmetics/helper.py
@@ -8,11 +8,17 @@ def numberToArray(number):
 class numberToArrayTests(unittest.TestCase):
 
 	def testSingleDigit(self):
-	  number = 3
-	  actual = numberToArray(number)
-	  expected = [3]
-	  self.failUnless(expected==actual)
+	  	number = 3
+	  	actual = numberToArray(number)
+	  	expected = [3]
+	  	self.failUnless(expected==actual)
 
+	def testDoubleDigit(self):
+		number = 84
+		actual = numberToArray(number)
+		expected = [8,4]
+		self.failUnless(expected=value)
+	
 def main():
     unittest.main()
 
