b57e8bd933128d063caf68823845402943821704
akatank <jk563htc@gmail.com>
2014-01-18 19:10:32 +0000

    Implemented multiplyOne.

--- integer_arithmetics/multiplyOne.py
+++ integer_arithmetics/multiplyOne.py
@@ -1,8 +1,38 @@
 import unittest
 import helpers.helper as helper
+import addition
 
 def multiplyOneListsToList(a,b):
-	return None
+	if len(b)!= 1:
+		return None
+
+	n = len(a)
+	c = [0]
+	d = []
+
+	a.reverse()
+
+	print a
+	print b
+	print c
+	print d
+
+	for i in range(n):
+		cAndDNum = a[i] * b[0]
+		cAndD = helper.numberToList(cAndDNum)
+		helper.prependZeros(cAndD,2)
+		c.append(cAndD[0])
+		d.append(cAndD[1])
+		print cAndDNum
+		print cAndD
+		print c 
+		print d
+	
+	c.reverse()
+	d.reverse()	
+	s = addition.addListsReturnList(c,d)
+	print s
+	return s	
 
 
 class MultiplyOneTests(unittest.TestCase):
@@ -17,10 +47,10 @@ class MultiplyOneTests(unittest.TestCase):
 	def testReturnsCorrectAnswerForTwoSingleDigits(self):
 		a = [4]
 		b = [6]
-		expected = [2,4]
-		actual = multiplyOnListsToList(a,b)
+		expected = [0,2,4]
+		actual = multiplyOneListsToList(a,b)
 		self.assertTrue(expected==actual)
-	
+		
 
 def main():
     unittest.main()
