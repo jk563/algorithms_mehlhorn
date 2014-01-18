d78cdac33313e81b4fd3e8790ffa4972bcb82980
akatank <jk563htc@gmail.com>
2014-01-18 22:14:13 +0000

    Implemented multiply and added a couple of tests to check.

--- integer_arithmetics/multiply.py
+++ integer_arithmetics/multiply.py
@@ -3,9 +3,31 @@ import addition
 import multiplyOne
 import helpers.helper as helper
 
-def multiplyListsReturnList(a,b):
-	return None
+def multiplyListsReturnList(listA,listB):
+	a = list(listA)
+	b = (listB)
+	s = []
+	n = len(a)
+	if len(b) > n:
+		n = len(b)
+	
+	helper.prependZeros(a,n)
+	helper.prependZeros(b,n)
+	
+	position = 0
 
+	b.reverse()
+
+	for i in range(n):
+		partial = multiplyOne.multiplyOneListsReturnList(a,[b[i]])
+		partial.reverse()
+		adjustmentForBase = len(helper.numberToList(helper.listToNumber(partial))) + position
+		helper.prependZeros(partial, adjustmentForBase)
+		partial.reverse()
+		s = addition.addListsReturnList(s,partial)
+		position = position + 1
+
+	return s
 
 class MultiplyTests(unittest.TestCase):
 	
@@ -16,8 +38,21 @@ class MultiplyTests(unittest.TestCase):
 		actual = multiplyListsReturnList(a,b)
 		self.assertTrue(expected==actual)
 
+	def testMultiplyDoubleDigits(self):
+		a = [3,4]
+		b = [8,7]
+		expected = [0,0,2,9,5,8]
+		actual = multiplyListsReturnList(a,b)
+		self.assertTrue(expected==actual)
+
+
 	def testMultiplyMultipleDigits(self):
-		pass
+		a = [1,4,3,3]
+		b = [8,5,7,9,2,4,7]
+		expected = [0,0,0,0,0,1,2,2,9,4,0,6,0,9,5,1]
+		actual = multiplyListsReturnList(a,b)
+		self.assertTrue(expected==actual)
+		
 
 def main():
 	unittest.main()
