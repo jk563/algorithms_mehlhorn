a5198d5b10c25d44ddbfefc4915f3fe6e7bf33f2
akatank <jk563htc@gmail.com>
2014-01-18 19:49:47 +0000

    Added framework for multiply.

---
+++ integer_arithmetics/multiply.py
@@ -0,0 +1,22 @@
+import unittest
+import addition
+import multiplyOne
+import helpers.helper as helper
+
+def multiplyListsReturnList(a,b):
+	return None
+
+
+class MultiplyTests(unittest.TestCase):
+	
+	def testMultiplySingleDigits(self):
+		pass
+
+	def testMultiplyMultipleDigits(self):
+		pass
+
+def main():
+	unittest.main()
+
+if __name__ == '__main__':
+	main()
